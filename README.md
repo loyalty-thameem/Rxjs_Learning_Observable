52b108d0685f5ffbd8b5daebe836ca0d78a11ac0
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-12-30 17:11:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Rxjs_Learning_Observable
-Created with CodeSandbox
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { Observable } from "rxjs";
+
+const observable = new Observable((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  subscriber.next(4);
+  setTimeout(() => {
+    subscriber.next(5);
+    subscriber.complete();
+  }, 3000);
+});
+
+console.log("Just before subscriber");
+
+observable.subscribe({
+  next(x) {
+    console.log("got value ", x);
+  },
+  error(err) {
+    console.log("Something wrong occurred", err);
+  },
+  complete() {
+    console.log("done");
+  }
+});
+console.log("Just after subscriber");
